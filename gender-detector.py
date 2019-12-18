6f36a52eeae1d5424d01b0694e5eacd30a619cd8
tomaszmjurek <tomaszmjurek@gmail.com>
2019-12-18 15:17:32 +0100

    Playing audio works!

--- gender-detector.py
+++ gender-detector.py
@@ -1,5 +1,11 @@
 import soundfile as sf
+import sounddevice as sd
+
+path = 'train/'
 
 #def get_data():
-wav_file = '001_K.wav'
-input_data, f_s = sf.read(wav_file)
+wav_file = path + '001_K.wav'
+input_data, f_s = sf.read(wav_file, dtype='float32')
+
+sd.play(input_data, f_s)
+status = sd.wait()
